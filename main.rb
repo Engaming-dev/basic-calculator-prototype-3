1275aa842068211d71d9115b045db9da2b7b7d78
Enes Gjikokaj <>
2021-06-01 13:20:25 +0000

    Added ability to subtract, moved into the alpha phase of development, and tweaked the code's string-to-variable merging.

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-puts " welcome to basic calculator prototype 5. NOTE: this is still a program in heavy development. currently only 4 number addition, multiplication and division(sadly only two numbers can be divided at a time currently) is supported.ALSO: In case you want to complete math operations with less than 4 numbers, in the numbers you dont need press enter."
+puts "basic calculator v.0.6. NOTE: all math operations work. but division currently only supports two number division at a time."
 puts "please enter your numbers."
 puts "a="
 a = gets.chomp.to_i
@@ -9,12 +9,16 @@ c = gets.chomp.to_i
 puts "d="
 d = gets.chomp.to_i
 final = a + b + c + d
-puts "FINAL RESULT:" + final.to_s
+puts "FINAL RESULT: #{final}"
+final_ab = a + b
+puts "FINAL RESULT (ONLY A AND B ADDED TOGETHER): #{final_ab} "
 finalmultiply = a * b * c * d
-puts "FINAL MULTIPLIED RESULT:" + finalmultiply.to_s
+puts "FINAL MULTIPLIED RESULT: #{finalmultiply}" 
 finalmultiplyab = a * b
-puts "FINAL MULTIPLIED RESULT (ONLY A AND B MULTIPLIED):" + finalmultiplyab.to_s
+puts "FINAL MULTIPLIED RESULT (ONLY A AND B MULTIPLIED): #{finalmultiplyab}"
 finaldivide_ab = a / b.to_f
 finaldivide_cd = c / d.to_f
 puts "FINAL DIVIDED RESULT(ONLY A AND B DIVIDED): #{finaldivide_ab}"
 puts "FINAL DIVIDED RESULT (ONLY D AND C DIVIDED): #{finaldivide_cd}"
+final_subtract = a - b - c - d
+puts "FINAL SUBTRACTED RESULT: #{final_subtract}"
