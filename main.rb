beb00dcaea1d8d43b9d21d8b13964c89e714d487
Enes Gjikokaj <>
2021-06-08 20:50:03 +0000

    This is a really buggy version. wanted to release it for help on fixing it for 1.0.0

--- main.rb
+++ main.rb
@@ -1,27 +1,20 @@
-puts "basic calculator v.0.8 beta. NOTE: Program is still in development and excpect a major rehaul."
+puts "basic calculator v.1.0 pre-release 1. NOTE: Program is still in development."
 puts "Instructions can be found in the instructions.md file."
-puts "please enter your numbers."
-puts "a="
-a = gets.chomp.to_i
-puts "b="
-b = gets.chomp.to_i
-puts "c="
-c = gets.chomp.to_i
-puts "d="
-d = gets.chomp.to_i
-final = a + b + c + d
-puts "FINAL RESULT: #{final}"
-final_ab = a + b
-puts "FINAL RESULT (ONLY A AND B ADDED TOGETHER): #{final_ab} "
-finalmultiply = a * b * c * d
-puts "FINAL MULTIPLIED RESULT: #{finalmultiply}" 
-finalmultiplyab = a * b
-puts "FINAL MULTIPLIED RESULT (ONLY A AND B MULTIPLIED): #{finalmultiplyab}"
-finaldivide = a / b / c / d.to_f
-finaldivide_ab = a / b.to_f
-finaldivide_cd = c / d.to_f
-puts "FINAL DIVIDED RESULT: #{finaldivide}"
-puts "FINAL DIVIDED RESULT(ONLY A AND B DIVIDED): #{finaldivide_ab}"
-puts "FINAL DIVIDED RESULT (ONLY D AND C DIVIDED): #{finaldivide_cd}"
-final_subtract = a - b - c - d
-puts "FINAL SUBTRACTED RESULT: #{final_subtract}"
+puts "How much numbers do you want to use today?
+  1 = 2 numbers
+  2 = 3 numbers (doesnt work yet)
+  3 = 4 numbers (doesnt work yet)"
+numbers = gets.chomp.to_i
+if numbers == 1
+  puts "what operator are we using?(only + works currently)"
+operator = gets.chomp.to_i
+if operator == +
+ puts "What are the numbers used?"
+ puts "a ="
+ number_a = gets.chomp.to_i
+ puts "b ="
+ number_b = gets.chomp.to_i
+ final = number_a + number_b
+ puts "FINAL RESULT: #{final}"
+else puts "Invalid."end
+else puts "Currently not available."end
