bdc2de74380bb6e8cf21f7868f67f57f8fad3339
Enes Gjikokaj <75257483+Engaming-dev@users.noreply.github.com>
2021-06-09 09:15:20 +0000

    Made the version usable. curently i only have options for 2 number addition.

--- main.rb
+++ main.rb
@@ -6,15 +6,13 @@ puts "How much numbers do you want to use today?
   3 = 4 numbers (doesnt work yet)"
 numbers = gets.chomp.to_i
 if numbers == 1
-  puts "what operator are we using?(only + works currently)"
-operator = gets.chomp.to_i
-if operator == +
- puts "What are the numbers used?"
- puts "a ="
- number_a = gets.chomp.to_i
- puts "b ="
- number_b = gets.chomp.to_i
- final = number_a + number_b
- puts "FINAL RESULT: #{final}"
-else puts "Invalid."end
-else puts "Currently not available."end
+  puts "what operator are we using?(only plus works currently)"
+ operator = gets.chomp
+ if operator == "plus"
+   puts "Enter your numbers."
+   number_a = gets.chomp.to_i
+   number_b = gets.chomp.to_i
+   final = number_a + number_b
+   puts "Final result: #{final}"
+ end
+end
