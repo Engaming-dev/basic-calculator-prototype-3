59a4561db1b418c972d99cda208af2ab43b17e30
Enes Gjikokaj <75257483+Engaming-dev@users.noreply.github.com>
2021-06-09 18:52:39 +0000

    This is the second commit of part 1. I will also merge this branch with master soon. added the ability to do 3 number addition.

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
-puts "basic calculator v.1.0 pre-release 1. NOTE: Program is still in development."
+puts "basic calculator v.1.0 pre-release 2. NOTE: Program is still in development."
 puts "Instructions can be found in the instructions.md file."
 puts "How much numbers do you want to use today?
   1 = 2 numbers
-  2 = 3 numbers (doesnt work yet)
+  2 = 3 numbers 
   3 = 4 numbers (doesnt work yet)"
 numbers = gets.chomp.to_i
 if numbers == 1
@@ -15,4 +15,14 @@ if numbers == 1
    final = number_a + number_b
    puts "Final result: #{final}"
  end
-end
+elsif numbers == 2
+  puts "What operator are we using?(Only plus works currently)"
+  operator = gets.chomp
+  if operator == "plus"
+    puts "Enter your numbers."
+    number_a2 = gets.chomp.to_i
+    number_b2 = gets.chomp.to_i
+    number_c = gets.chomp.to_i
+    final2 = number_a2 + number_b2 + number_c
+    puts "Final result: #{final2}"end
+  end
